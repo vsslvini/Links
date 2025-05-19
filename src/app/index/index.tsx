d17b7e4aef92c076e7edb15e72c8535a33f61fbd
Vinicius <vsslviniciussousa@gmail.com>
2025-05-19 17:52:02 -0400

    Criando estrutura do Header

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native"
+import { View, Image, TouchableOpacity} from "react-native";
+import {styles} from "./styles"
+import { MaterialIcons } from "@expo/vector-icons"
+import { colors } from "@/styles/colors";
 
 export default function Index() {
     return (
-        <View>
-            <Text>Hello React Native</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.png")} style={styles.logo} />
+                <TouchableOpacity activeOpacity={0.3}>
+                <MaterialIcons name="add" size={32} color={colors.green[300]} />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
