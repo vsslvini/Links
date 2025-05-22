3415ae62b563bca047b95c0aedb40f4d6a1e77eb
Vinicius <vsslviniciussousa@gmail.com>
2025-05-22 10:15:30 -0400

    Instalando o async storage

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 
@@ -24,7 +24,19 @@ export default function Add() {
     const [url, setUrl] = useState("");
 
     function handleAdd() {
-        console.log({ name, url })
+        if (!category) {
+            return Alert.alert("Categoria", "Selecione a categoria")
+        }
+        if (!name.trim()) {
+            return Alert.alert("Nome", "Insira um nome válido")
+        }
+        if (!url.trim()) {
+            return Alert.alert("Url", "Insira uma URL válida")
+        }
+
+
+
+        console.log({ category, name, url })
     }
 
     return (
@@ -41,7 +53,7 @@ export default function Add() {
 
             <View style={styles.form}>
                 <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
-                <Input placeholder="Url" onChangeText={setUrl} autoCorrect={false} />
+                <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false} />
                 <Button title="Adicionar" onPress={handleAdd} />
             </View>
         </View>
